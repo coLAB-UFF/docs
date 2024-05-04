25e3afd95fda055e68ed5285941121717b24f4bf
Viktor Chagas <30334674+ombudsmanviktor@users.noreply.github.com>
2024-05-04 15:04:37 -0300

    Update dataviz_07_gabaritos.R

--- dataviz_07_gabaritos.R
+++ dataviz_07_gabaritos.R
@@ -47,26 +47,53 @@ bbb21_mensagens
 
 # Procure quantas vezes Juliette é mencionada nesse conjunto de mensagens.
 
+## OPÇÃO A
+
 bbb21_mensagens$text %>% 
   str_detect("Juliette|Juliete|juliette|juliete") %>% 
   sum(TRUE)
+
+## OPÇÃO B
 
 bbb21_mensagens %>% 
   mutate(new_text = str_detect(text, "Juliette|Juliete|juliette|juliete")) %>% 
   filter(new_text == "TRUE") %>% 
   count()
 
+## OPÇÃO C
+
+bbb21_mensagens %>% 
+  filter(grepl("Juliette", text)) %>% count()
+
 # Substitua todas as menções a Bolsonaro (ou bolsonaro ou BOLSONARO) por Bozo
+
+## OPÇÃO A
 
 bbb21_mensagens$text <- bbb21_mensagens$text %>% 
   str_replace("Bolsonaro|bolsonaro|BOLSONARO", "Bozo")
+
+## OPÇÃO B
 
 bbb21_mensagens$text <- bbb21_mensagens$text %>% 
   str_replace("Bolsonaro", "Bozo") %>% 
   str_replace("bolsonaro", "Bozo") %>% 
   str_replace("BOLSONARO", "Bozo")
 
+## OPÇÃO C
+
+bozo <- bbb21_mensagens %>%
+  #filter(grepl("Bolsonaro|bolsonaro|BOLSONARO", text)) %>% 
+  mutate(text = gsub("Bolsonaro", "Bozo", text)) %>% 
+  mutate(text = gsub("bolsonaro", "Bozo", text)) %>% 
+  mutate(text = gsub("BOLSONARO", "Bozo", text))
+
 # Transforme todos os caracteres das mensagens nos tweets em minúsculas.
+
+## OPÇÃO A
+
+bbb21_mensagens %>% mutate(text = stringr::str_to_lower(text))
+
+## OPÇÃO B
 
 bbb21_mensagens$text <- bbb21_mensagens$text %>% 
   str_to_lower()
